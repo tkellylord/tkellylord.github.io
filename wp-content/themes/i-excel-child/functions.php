6093137fce8af4d9cd225c0ad2a43c059c5e4300
Tom Kelly-Lord <tomkelly-lord@hotmail.co.uk>
2018-01-11 15:01:38 +0000

    removed
    removed.

--- wp-content/themes/i-excel-child/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function custom_add_google_fonts() {
- wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Nunito:400,400i,700', false );
- }
- add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
